dabb17f6e0ab6660bfa9a4e99a4cd73e4f587e86
lizhiliang <lzl102872@163.com>
2026-02-25 16:40:33 +0800

    feat: 添加声明式信息图demo

--- project/infographic/src/App.vue
+++ project/infographic/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/infographic">Infographic</RouterLink>
       </nav>
     </div>
   </header>
---
+++ project/infographic/src/views/InfographicView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div ref="containerRef"></div>
+  </template>
+  
+  <script setup>
+  import {ref, onMounted, onBeforeUnmount} from 'vue';
+  import {Infographic} from '@antv/infographic';
+  
+  const containerRef = ref(null);
+  let infographic = null;
+  
+  onMounted(() => {
+    infographic = new Infographic({
+      container: containerRef.value,
+      width: '100%',
+      height: '100%',
+    });
+  
+    infographic.render(`
+      infographic list-row-simple-horizontal-arrow
+      data
+        lists
+          - label 步骤 1
+            desc 开始
+          - label 步骤 2
+            desc 进行中
+          - label 步骤 3
+            desc 完成
+    `);
+  });
+  
+  onBeforeUnmount(() => {
+    if (infographic) {
+      infographic.destroy();
+    }
+  });
+  </script>
