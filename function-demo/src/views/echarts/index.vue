f8c4771727b69b7cf880a5b991adad61ae92c07f
lizhiliang <lzl102872@163.com>
2026-03-10 17:52:59 +0800

    feat: 添加echartsDemo

--- function-demo/src/views/echarts/index.vue
+++ function-demo/src/views/echarts/index.vue
@@ -1,2 +1,15 @@
+<template>
+  <div>
+    Echarts Demo
+  </div>
+</template>
 
+<script setup lang="ts">
+// import { ref } from 'vue';
 
+// const feature = ref();
+</script>
+
+<style lang="css" scoped>
+
+</style>
